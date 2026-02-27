536322aa8ab327763c13451f528a46a05070bf78
ggreen <gregoryg@vmware.com>
2026-02-27 13:47:21 -0500

    adding rabbit script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# spring-for-rabbit-super-stream-and-sql-filters
+# spring-for-rabbit-super-stream-and-esql-filters
+
+Start RabbitMQ
+
+```shell
+deployment/local/containers/rabbit.sh
+```
--- applications/publisher/src/main/java/io/cloudNativeData/spring/rabbit/streams/SpringRabbitStreamsApp.java
+++ applications/publisher/src/main/java/io/cloudNativeData/spring/rabbit/streams/SpringRabbitStreamsApp.java
@@ -1,9 +1,7 @@
 package io.cloudNativeData.spring.rabbit.streams;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRabbitStreamsApp {
