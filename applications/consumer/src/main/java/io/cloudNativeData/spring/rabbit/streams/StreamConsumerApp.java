4c3680ed508b083a77a2701d92f40d502edf1904
ggreen <gregoryg@vmware.com>
2026-02-27 13:51:29 -0500

    adding sub projects

---
+++ applications/consumer/src/main/java/io/cloudNativeData/spring/rabbit/streams/StreamConsumerApp.java
@@ -0,0 +1,14 @@
+package io.cloudNativeData.spring.rabbit.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamConsumerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamConsumerApp.class, args);
+	}
+
+
+}
--- applications/publisher/src/main/java/io/cloudNativeData/spring/rabbit/streams/StreamPublisherApp.java
+++ applications/publisher/src/main/java/io/cloudNativeData/spring/rabbit/streams/StreamPublisherApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRabbitStreamsApp {
+public class StreamPublisherApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRabbitStreamsApp.class, args);
+		SpringApplication.run(StreamPublisherApp.class, args);
 	}
 
 
