75c083c4d435f4b47c7baff5f8df28ef7be2d740
ggreen <gregoryg@vmware.com>
2026-03-05 21:35:56 -0500

    cleanup

--- applications/consumer/src/main/java/io/cloudNativeData/spring/rabbit/streams/StreamConsumerApp.java
+++ applications/consumer/src/main/java/io/cloudNativeData/spring/rabbit/streams/StreamConsumerApp.java
@@ -10,5 +10,4 @@ public class StreamConsumerApp {
 		SpringApplication.run(StreamConsumerApp.class, args);
 	}
 
-
 }
--- components/domains/src/main/java/io/cloudNativeData/spring/rabbit/streams/domain/Event.java
+++
@@ -1,7 +0,0 @@
-package io.cloudNativeData.spring.rabbit.streams.domain;
-
-import lombok.Builder;
-
-@Builder
-public record Event(String id, long timestamp, String message, short priority) {
-}
